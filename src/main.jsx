9e5dd45ab7ec3d7c5638aa56c46092483b4eb4bb
Ayan Upadhaya <ayanu881@gmail.com>
2023-04-19 10:42:32 -0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>,
+      },
+      {
+        path:'/login',
+        element:<Login></Login>
+      },
+      {
+        path:'/register',
+        element:<Register></Register>
+      },
+    ]
+  }
+])
+import './index.css'
+import Main from './Layout/Main';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}></RouterProvider>
+)
